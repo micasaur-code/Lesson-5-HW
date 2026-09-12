8489e6f850d0aa24f1c11d80272131e0a9e95f68
micasaur-code <micap112@gmail.com>
2026-09-12 18:40:48 +0300

    Add files via upload

---
+++ lesson5.py
@@ -0,0 +1,40 @@
+
+#########################
+# Ex 1 #
+#########################
+
+print("My name is Mica")
+print("I live in Kfar Sava")
+print("I love making music")
+
+
+#########################
+# Ex 2 #
+#########################
+
+color = input("What is your favorite color? ")
+print(color, "is a beautiful color!")
+
+
+
+#########################
+# Ex 3 #
+#########################
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print(f"Hello {first_name} {last_name}, welcome to python!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
